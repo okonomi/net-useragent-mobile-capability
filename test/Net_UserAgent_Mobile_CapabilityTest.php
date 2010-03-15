96dd577eaeb10793b15bc3b44b1d9d78a45686d6
okonomi <okonomi@224e63c0-5457-0410-989b-907d9cf58824>
2010-03-15 16:15:27 +0000

    テストの事前事後処理を追加
    git-svn-id: http://svn.openpear.org/Net_UserAgent_Mobile_Capability/trunk@1704 224e63c0-5457-0410-989b-907d9cf58824

--- test/Net_UserAgent_Mobile_CapabilityTest.php
+++ test/Net_UserAgent_Mobile_CapabilityTest.php
@@ -8,6 +8,22 @@ require_once 'Net/UserAgent/Mobile.php';
 
 class Net_UserAgent_Mobile_CapabilityTest extends PHPUnit_Framework_TestCase
 {
+    private $_backup;
+
+
+    public function setUp()
+    {
+        $this->_backup = array(
+            $_SERVER,
+            $_REQUEST,
+        );
+    }
+
+    public function tearDown()
+    {
+        list ($_SERVER, $_REQUEST) = $this->_backup;
+    }
+
     public function testGetBrowserHtmlTable()
     {
         $_SERVER['HTTP_USER_AGENT'] = 'DoCoMo/2.0 P906i(c100;TB;W24H15)';
