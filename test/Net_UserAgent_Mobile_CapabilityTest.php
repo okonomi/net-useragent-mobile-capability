3a3f75e7a6602aca16b966b3daba8532c479db89
okonomi <okonomi@224e63c0-5457-0410-989b-907d9cf58824>
2010-03-16 04:13:56 +0000

    テストをまとめた
    git-svn-id: http://svn.openpear.org/Net_UserAgent_Mobile_Capability/trunk@1706 224e63c0-5457-0410-989b-907d9cf58824

--- test/Net_UserAgent_Mobile_CapabilityTest.php
+++ test/Net_UserAgent_Mobile_CapabilityTest.php
@@ -8,12 +8,30 @@ require_once 'Net/UserAgent/Mobile.php';
 
 class Net_UserAgent_Mobile_CapabilityTest extends PHPUnit_Framework_TestCase
 {
-    public function testGetBrowserHtmlTable()
+    public function testGet()
     {
-        $_SERVER['HTTP_USER_AGENT'] = 'DoCoMo/2.0 P906i(c100;TB;W24H15)';
-        $ua = Net_UserAgent_Mobile::factory();
-        $cap = new Net_UserAgent_Mobile_Capability($ua);
+        $fixture = array(
+            array(
+                'device'    => 'D501i',
+                'useragent' => 'DoCoMo/1.0/D501i',
+                'property'  => array(
+                    'browser.html.table'        => false,
+                    'browser.html.css'          => false,
+                    'browser.html.css.external' => false,
+                    'device.flash'              => false,
+                ),
+            ),
+        );
 
-        $this->assertEquals($cap->get('browser.html.table'), true);
+        foreach ($fixture as $data) {
+            $_SERVER['HTTP_USER_AGENT'] = $data['useragent'];
+
+            $ua = Net_UserAgent_Mobile::factory();
+            $cap = new Net_UserAgent_Mobile_Capability($ua);
+
+            foreach ($data['property'] as $name => $value) {
+                $this->assertEquals($cap->get($name), $value);
+            }
+        }
     }
 }
